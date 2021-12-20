ca994fa9d6fa0f45c03a80f74ed271b2cfa22624
TiagoCaetanoReal <tiagomiguelbaraocaetano@gmail.com>
2021-12-20 12:49:27 +0000

    message_for_chico

--- src/poo/Main.java
+++ src/poo/Main.java
@@ -3,12 +3,7 @@ package poo;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("asfasfasfg");
-=======
 		System.out.println("you are gey, you are transgender");
->>>>>>> 84ec456a3a411a29bde5013e83c310de14090823
-
 	}
 
 }

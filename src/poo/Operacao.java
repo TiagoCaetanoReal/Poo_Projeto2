34a4ad98b2a6fec85226381b0de1cc6f405e3ef6
TiagoCaetanoReal <tiagomiguelbaraocaetano@gmail.com>
2021-12-22 23:18:31 +0000

    added arrayList<HashMap<>> content

--- src/poo/Operacao.java
+++ src/poo/Operacao.java
@@ -1,13 +1,12 @@
 package poo;
 
-
 import java.io.*;
 import java.util.*;
 
 public class Operacao {
 	 
 	String nomeFicheiro;
-	ArrayList<HashMap <String , String>> tabela = new ArrayList<HashMap<String, String>>();
+	ArrayList<HashMap <String , String>> tabela = new ArrayList<>();
 	String[] conteudoColuna;
 	String query;
 	
@@ -17,29 +16,45 @@ public class Operacao {
 	}
 	
 	String getNomeFicheiro() { return this.nomeFicheiro;}
-	void setNomeFicheiro(String nomeFicheiro) { this.nomeFicheiro=nomeFicheiro;}
+	private void setNomeFicheiro(String nomeFicheiro) { this.nomeFicheiro=nomeFicheiro;}
 	
 	String[] getConteudoColuna() { return this.conteudoColuna;}
-	void setConteudoColuna(String[] conteudoColuna) { this.conteudoColuna=conteudoColuna;}
+	private void setConteudoColuna(String[] conteudoColuna) { this.conteudoColuna=conteudoColuna;}
 	
 	void lerFicheiro(){
 		try{
-			
 			FileReader ficheiro = new FileReader(System.getProperty("user.dir") + "/src/" + nomeFicheiro); 
-			BufferedReader buffer = new BufferedReader(ficheiro);
+			//BufferedReader buffer = new BufferedReader(ficheiro);
+			Scanner myReader = new Scanner(ficheiro);
 			
-			String[] colunas = buffer.readLine().split(";");
+			String[] colunas = myReader.nextLine().split(",");
 			setConteudoColuna(colunas);
 			
 			for(String a : conteudoColuna) {
-				System.out.println(a);
+				System.out.print(a + " | ");
 			}
 			
-		} catch(FileNotFoundException e){
+			int j = 0;
+			while(myReader.hasNextLine()){
+				String[] linha = myReader.nextLine().split(",");
+				HashMap<String , String> data = new HashMap<>();
+				
+				for(int i = 0; i < linha.length; i++) {
+					data.put(conteudoColuna[i],linha[i]);
+					System.out.println(conteudoColuna[i] + " " + linha[i]);
+					System.out.println(i);
+				}
+				tabela.add(data);
+				j++;
+				break;
+			}
+			System.out.println("\n" + tabela.get(0));
+		
+		}catch(FileNotFoundException e){
 			e.printStackTrace();
-		}
-		  catch(IOException e){
+		}catch(IOException e){
 		    e.printStackTrace();
 		  }
+		
 	 }
 }
